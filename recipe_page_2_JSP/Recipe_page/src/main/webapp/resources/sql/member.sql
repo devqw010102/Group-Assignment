c06a61194cd235ebd28d09ebb35afd0d1e1cd08c
devqw010102 <qw010102@gmail.com>
2025-11-26 14:24:31 +0900

    Add files via upload

--- recipe_page_2_JSP/Recipe_page/src/main/webapp/resources/sql/member.sql
+++ recipe_page_2_JSP/Recipe_page/src/main/webapp/resources/sql/member.sql
@@ -2,7 +2,7 @@ CREATE TABLE member(
     id VARCHAR2(20) PRIMARY KEY,
     password VARCHAR2(20) NOT NULL,
     name VARCHAR2(10) NOT NULL,
-    gender VARCHAR(4),
+    gender VARCHAR2(10),
     birth VARCHAR2(10),
     mail VARCHAR2(30),
     phone VARCHAR2(20),
@@ -17,7 +17,16 @@ DESC member;
 INSERT INTO member VALUES('admin', 'admin1234', '관리자', null, null, null, null, null, null);
 INSERT INTO member(id, password, name, gender, birth, mail, phone, address) VALUES('TEST', 'TEST1234', '테스트', '남', '2001-01-02', 'test@gmail.com', '010-1234-5678', '울산');
 
+SELECT * FROM MEMBER WHERE ID = 'admin';
+
+UPDATE MEMBER SET gender = '미입력' WHERE ID = 'test';
+
+
+
+ALTER TABLE member
+    MODIFY gender VARCHAR2(10);
+
 commit;
 
-DROP TABLE MEMBER;
+--DROP TABLE MEMBER;
 
